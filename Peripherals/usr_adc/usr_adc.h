1143c883ba45dd19cba8c3fc81678a4c9a2223be
Paul Timke <87957114+paultimke@users.noreply.github.com>
2021-10-09 14:08:31 -0600

    Fix type mistake

--- Peripherals/usr_adc/usr_adc.h
+++ Peripherals/usr_adc/usr_adc.h
@@ -16,7 +16,7 @@
 #define ADC_VREF                1100
 #define ADC_MAX_VALUE           4095
 #define ADC_SAMPLE_COUNT        10
-#define ADC_CALIBRATION ON      1
+#define ADC_CALIBRATION_ON      1
 
 /*-------------------------Prototypes-------------------------*/
 
